81e1d414c023a491219e67593f134b91c978a7e8
U-LIVINGROOM\Gingerbred Man <Gingerbred Man@LIVINGROOM.(none)>
2010-06-10 11:21:29 -0500

    Convert files to UNIX format. Fix SDL_SetVideoMode() fails on webOS device. Add LICENSE file. Add GPL heading to all .cpp files. Fix uninitialized boolean Running, member of Game class (game ends on startup when compiled in release mode). 25ms sleep added to main loop. Some debugging output added. Added buildit.cmd batch file for building using PDK on Windows.

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,42 @@
+/*
+This file is part of SDL Mille.
+
+SDL Mille is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SDL Mille is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SDL Mille.  If not, see <http://www.gnu.org/licenses/>.
+
+(See file LICENSE for full text of license)
+*/
+
 #include "Game.h"
 
 using namespace _SDLMille;
 
 int	main	(int argc, char* argv[])
 {
+	printf("Started.");
+	
 	try
 	{
 		Game	TheGame;
 
 		// Return 0 if the game executed OK. Otherwise, return -1.
+		printf("Trying to execute game.");
 		return (TheGame.OnExecute()) ? 0 : -1;
 	}
-	catch (int e) {}
+	catch (int e)
+	{
+		printf("Error number %u caught.", e);
+	}
 
 	return 0;
 }
